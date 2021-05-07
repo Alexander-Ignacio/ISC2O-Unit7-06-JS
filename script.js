ac4bf717902357da1becc5eb5d86e8df43f872a3
AlexanderIgnac1 <>
2021-05-07 15:09:09 +0000

    parseInt. Need I say more?

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let guess = 0
 while (guess !== age) {
   guess = prompt('What is my age?')
 
+  guess = parseInt(guess)
+
   if (guess === age) {
     alert('You got it right! Do you want to play again (y/n)')
   } else if (guess < age) {
